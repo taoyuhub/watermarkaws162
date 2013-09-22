a0aa4b16ebaff82ceca8242876a6c245a915f009
taoyu <yutaohc@yahoo.com>
2013-09-22 10:13:43 -0400

    Initial commit fish/test1.php

---
+++ fish/test1.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'config.inc.php';
+require_once 'util.inc.php';
+require_once 'AWSSDKforPHP/sdk.class.php';
+
+$ec2 = new AmazonEC2();
+$ec2->set_region(AmazonEC2::REGION_OREGON);
+$ec2_describe_response = $ec2->describe_instances();
+
+var_dump($ec2_describe_response);
