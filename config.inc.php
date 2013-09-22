a4693181476a4874631e605ac461ea1a202f8ff0
taoyu <yutaohc@yahoo.com>
2013-09-22 09:24:59 -0400

    Initial commit

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Local configuration; requires editing.
+ */
+
+define('UARWAWS_S3_BUCKET' , '');
+define('UARWAWS_SDB_DOMAIN' , '');
+define('UARWAWS_SQS_QUEUE' , '');
+define('UARWAWS_SNS_TOPIC' , '');
