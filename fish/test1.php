662fa8f57933758aacb1ab0ba7f454a6cda14842
taoyu <yutaohc@yahoo.com>
2013-09-22 10:24:52 -0400

    Update fish/test1.php

--- fish/test1.php
+++ fish/test1.php
@@ -6,4 +6,6 @@ $ec2 = new AmazonEC2();
 $ec2->set_region(AmazonEC2::REGION_OREGON);
 $ec2_describe_response = $ec2->describe_instances();
 
-var_dump($ec2_describe_response->body->reservationSet->item);
+foreach($ec2_describe_response->body->reservationSet->item as $item) {
+	var_dump($ec2_describe_response->body->reservationSet->item);
+}
