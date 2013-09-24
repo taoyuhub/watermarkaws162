02f1377cb39d4940efdfb9f727c4c4b3d5a97190
taoyu <yutaohc@yahoo.com>
2013-09-23 21:17:05 -0400

    Update test_carousel.php

--- fish/test_carousel.php
+++ fish/test_carousel.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'config.inc.php';
-require_once 'util.inc.php';
+require_once '../config.inc.php';
+require_once '../util.inc.php';
 require_once 'AWSSDKforPHP/sdk.class.php';
 
 $sdb = new $AmazonSDB();
