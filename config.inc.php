1d20f6213a83b620fbcd8affe00338a2c1639c89
taoyu <yutaohc@yahoo.com>
2013-09-24 21:46:54 -0400

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -11,7 +11,8 @@ define('UARWAWS_SQS_QUEUE' , 'images-to-watermark');
 define('UARWAWS_SNS_TOPIC' , 'watermark-bad-upload');
 */
 
-define('AWS_S3_BUCKET' , 'taoyu.test.watermark');
+//define('AWS_S3_BUCKET' , 'taoyu.test.watermark');
+define('AWS_S3_BUCKET' , '');
 define('AWS_SDB_DOMAIN' , 'watermarkedimages');
 define('AWS_SQS_QUEUE' , 'images-to-watermark');
 define('AWS_SNS_TOPIC' , 'watermark-bad-upload');
