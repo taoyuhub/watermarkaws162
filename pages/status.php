c8d41ca618ef355df3042d4188d9a747b27f37bd
taoyu <yutaohc@yahoo.com>
2013-09-24 21:38:20 -0400

    Update status.php to display info in a table

--- pages/status.php
+++ pages/status.php
@@ -4,69 +4,159 @@
  * Overview of configuration and status.
  */
 
+echo '<div class="row-fluid">';
+echo    '<div class="span2">';
+echo    '</div>';
+echo    '<div class="span8">';
+echo    '<table class="table table-bordered table-hover responsive-utilities">';
+echo        '<thead>';
+echo            '<tr>';
+echo                '<th>No.</th>';
+echo                '<th>Services & Tool</th>';
+echo                '<th>Status / Configuration</th>';
+echo            '</tr>';
+echo        '</thead>';
+echo        '<tbody>';
+
 // Check local config.inc.php for completeness.
 if (!AWS_S3_BUCKET) {
+/*	
   echo renderMsg('error', array(
     'heading' => 'S3 Bucket name missing!',
     'body' => 'Edit config.inc.php in siteroot and specify the name of the bucket in AWS_S3_BUCKET.',
   ));
+ */ 
+  echo '<tr class="error">';
+  echo    '<td>1</td>';
+  echo    '<td>Amazon S3</td>';
+  echo    '<td>S3 Bucket name missing! Edit config.inc.php in siteroot and specify the name of the bucket in AWS_S3_BUCKET. </td>';
+  echo '</tr>';
 }
 else {
+/*	
   echo renderMsg('success', array(
     'heading' => 'S3 Bucket name found:',
     'body' => AWS_S3_BUCKET,
   ));
+ */ 
+  echo '<tr class="success">';
+  echo    '<td>1</td>';
+  echo    '<td>Amazon S3</td>';
+  echo    '<td>S3 Bucket name: <strong>'.AWS_S3_BUCKET.'</strong></td>';
+  echo '</tr>';
 }
 
 if (!AWS_SQS_QUEUE) {
+/*	
   echo renderMsg('error', array(
     'heading' => 'SQS Queue missing!',
     'body' => 'Edit config.inc.php in siteroot and specify the name of the queue in AWS_SQS_QUEUE.',
   ));
+ */ 
+  echo '<tr class="error">';
+  echo    '<td>2</td>';
+  echo    '<td>Amazon SQS</td>';
+  echo    '<td>SQS Queue missing! Edit config.inc.php in siteroot and specify the name of the bucket in AWS_SQS_QUEUE. </td>';
+  echo '</tr>';
 }
 else {
+/*	
   echo renderMsg('success', array(
     'heading' => 'SQS Queue name found:',
     'body' => AWS_SQS_QUEUE,
   ));
+ */ 
+  echo '<tr class="success">';
+  echo    '<td>2</td>';
+  echo    '<td>Amazon SQS</td>';
+  echo    '<td>SQS Queue name: <strong>'.AWS_SQS_QUEUE.'</strong></td>';
+  echo '</tr>';
 }
 
 if (!AWS_SDB_DOMAIN) {
+/*	
   echo renderMsg('error', array(
     'heading' => 'SimpleDB domain missing!',
     'body' => 'Edit config.inc.php in siteroot and specify the name of the domain in AWS_SDB_DOMAIN.',
   ));
+ */ 
+  echo '<tr class="error">';
+  echo    '<td>3</td>';
+  echo    '<td>Amazon SDB</td>';
+  echo    '<td>SimpleDB domain missing! Edit config.inc.php in siteroot and specify the name of the bucket in AWS_SDB_DOMAIN.</td>';
+  echo '</tr>';
 }
 else {
+/*	
   echo renderMsg('success', array(
     'heading' => 'SimpleDB domain name found:',
     'body' => AWS_SDB_DOMAIN,
   ));
+ */ 
+  echo '<tr class="success">';
+  echo    '<td>3</td>';
+  echo    '<td>Amazon SDB</td>';
+  echo    '<td>SimpleDB domain name: <strong>'.AWS_SDB_DOMAIN.'</strong></td>';
+  echo '</tr>';
 }
 
 if (!AWS_SNS_TOPIC) {
+/*	
   echo renderMsg('error', array(
     'heading' => 'Simple Notification Service topic name missing!',
     'body' => 'Edit config.inc.php in siteroot and specify the name of the topic in AWS_SNS_TOPIC.',
   ));
+ */ 
+  echo '<tr class="error">';
+  echo    '<td>4</td>';
+  echo    '<td>Amazon SNS</td>';
+  echo    '<td>Simple Notification Service topic name missing! Edit config.inc.php in siteroot and specify the name of the bucket in AWS_SNS_TOPIC.</td>';
+  echo '</tr>';
 }
 else {
+/*
   echo renderMsg('success', array(
     'heading' => 'Simple Notification Service topic name found:',
     'body' => AWS_SNS_TOPIC,
   ));
+ */
+  echo '<tr class="success">';
+  echo    '<td>4</td>';
+  echo    '<td>Amazon SNS</td>';
+  echo    '<td>Simple Notification Service topic name: <strong>'.AWS_SNS_TOPIC.'</strong></td>';
+  echo '</tr>';
 }
 
 // Check if ImageMagick is installed.
 if (!extension_loaded('imagick')) {
+/*	
   echo renderMsg('error', array(
     'heading' => 'ImageMagick is not installed!',
     'body' => 'Images cannot be uploaded and processed without ImageMagick.',
   ));
+ */ 
+  echo '<tr class="error">';
+  echo    '<td>5</td>';
+  echo    '<td>ImageMagick</td>';
+  echo    '<td>ImageMagick is not installed! Images cannot be uploaded and processed without ImageMagick.</td>';
+  echo '</tr>';
 }
 else {
-  echo renderMsg('success', 'ImageMagick is installed.');
+  //echo renderMsg('success', 'ImageMagick is installed.');
+  
+  echo '<tr class="success">';
+  echo    '<td>5</td>';
+  echo    '<td>ImageMagick</td>';
+  echo    '<td>ImageMagick is installed</td>';
+  echo '</tr>';
 }
+
+echo       '</tbody>';
+echo     '</table>';
+echo   '</div>';
+echo   '<div class="span2">';
+echo   '</div>';
+echo '</div>';
 
 // Try to connect to Amazon EC2.
 try {
