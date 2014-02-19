f67824f47fd2e8e7f44d23b0124f063a6e546b33
taoyu <taoyu@nowhere.com>
2014-02-19 14:51:45 -0500

    add comment in config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -4,7 +4,7 @@
  * Local configuration; requires editing.
  */
 
-/*
+/***
 define('UARWAWS_S3_BUCKET' , 'taoyu.test.watermark');
 define('UARWAWS_SDB_DOMAIN' , 'watermarkedimages');
 define('UARWAWS_SQS_QUEUE' , 'images-to-watermark');
